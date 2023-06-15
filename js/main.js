72a874c86ea7382d8649adb3574ee17033ff0471
Armando <amcontreras10@gmail.com>
2023-06-15 11:09:18 -0400

    commented entire project

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
+// API Key from giphy.com
 let APIKEY = "o6qLzjsOJyGVBd083ZbXLhtuvftVgGPF";
+// webpage loaded
 document.addEventListener("DOMContentLoaded", init);
+// click event to get content
 function init() {
     document.getElementById("btnSearch").addEventListener("click", ev => {
         ev.preventDefault(); //to stop the page reload
+        // url info from GIPHY Search Endpoint 
         let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=9&q=`;
         let str = document.getElementById("search").value.trim();
         url = url.concat(str);
         console.log(url);
+        // get response and process data
         fetch(url)
             .then(response => response.json())
             .then(content => {
                 //data, pagination, meta
                 console.log(content.data);
                 console.log("META", content.meta);
+                // creating elements from data
                 let fig = document.createElement("figure");
                 let img = document.createElement("img");
                 let fc = document.createElement("figcaption");
@@ -22,9 +28,12 @@ function init() {
                 fig.appendChild(img);
                 fig.appendChild(fc);
                 let out = document.querySelector(".out");
+                //newest search goes to the top
                 out.insertAdjacentElement("afterbegin", fig);
+                //clear up the form in the console
                 document.querySelector("#search").value = "";
             })
+            // handle any errors
                 .catch (err => {
                 console.error(err);
             });
